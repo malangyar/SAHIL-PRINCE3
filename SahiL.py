77ca72ab22211921864e2ca07e8f7b512a8413e1
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-06-03 14:55:30 +0500

    Create SahiL.py

---
+++ SahiL.py
@@ -0,0 +1,9 @@
+import os
+os.system('git pull')
+os.system('termux-setup-storage')
+print("\033[1;32m WELCOME TO MY TOLL")
+print("\033[1;32m JALNE WALO JALNA NHI")
+print("\033[1;32m  This Tool Is Cooded By Sahil-Prince")
+print("\033[1;32m  Haters Papa Bolne Ka Liye Ready Ho Jao")
+import app
+Main()
